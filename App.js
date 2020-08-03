d1884da3042a94657cf8001aeec3d65d82c31dbc
andre.machado <4ndre.machado@gmail.com>
2020-08-03 16:17:00 -0300

    Navigation
    1> npx expo-cli init shows-app
    2> (da enter em blank mesmo)
     Bare workflow é aquele react native padrãozão
    3> cd shows-app
    4> yarn android
    5> abre outro terminal nesse diretorio e coloca o comando:
        yarn add @react-navigation/native
    6> comando grande: expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
    7> dê o comando: yarn add @react-navigation/stack
    8>Codigo (criar pasta src, com screens e Home)
    
    8.1 - Criando Home.js)
    
    const HomeScreen = () => {
      return (
    		//parte 1, retornar algo
      )
    }
    
    //2, exportar
    export default HomeScreen;
    
    const styles = StyleSheet.create({
    	//3 - estilos
    })
    
    8.2- Terminando Home)
    
    import React from 'react';
    import { StyleSheet, View, Text } from 'react-native';
    
    const HomeScreen = () => {
      return (
        <View style={styles.viewStyle}>
          <Text>Heae World!</Text>
        </View>
      );
    };
    
    export default HomeScreen;
    
    const styles = StyleSheet.create({
      viewStyle: {
        flex: 1,
        alignItems: 'center',
        justifyContent: 'center',
      },
    });

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+///////////acima é o que foi gerado automático////
+//criando do zero:
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/Home';
 
-export default function App() {
+function App() {
+  const Stack = createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Séries inc." component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
