c71bea73269bfdd210b1312c24e05ab866b2e432
andre.machado <4ndre.machado@gmail.com>
2020-08-04 11:57:41 -0300

    Barra alterada para ícones Foi feito isso pela adição de um novo componente: TouchableOpacity Com um estilo para posicionar os ícones corretamente na barra

--- App.js
+++ App.js
@@ -4,13 +4,38 @@ import { NavigationContainer } from '@react-navigation/native';
 import NewShowScreen from './src/screens/NewShow';
 import ShowsScreen from './src/screens/Shows';
 import HomeScreen from './src/screens/Home';
+import { TouchableOpacity, StyleSheet, View } from 'react-native';
+import { Octicons } from '@expo/vector-icons';
 
 const TabNav = createBottomTabNavigator();
+
+const tabBarStyles = StyleSheet.create({
+  viewStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+});
+
+const TabBar = ({ navigation }) => {
+  return (
+    <View style={tabBarStyles.viewStyle}>
+      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <Octicons name="home" size={30} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('Shows')}>
+        <Octicons name="list-unordered" size={30} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('NewShow')}>
+        <Octicons name="diff-added" size={30} color="black" />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 function App() {
   return (
     <NavigationContainer>
-      <TabNav.Navigator>
+      <TabNav.Navigator tabBar={(props) => <TabBar {...props} />}>
         <TabNav.Screen name={'Home'} component={HomeScreen} />
         <TabNav.Screen name={'Shows'} component={ShowsScreen} />
         <TabNav.Screen name={'NewShow'} component={NewShowScreen} />
